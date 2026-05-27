e1937cf0c73b51e425f74103d61ea117806b4177
Emad <32420327+Emad-Almuzaini@users.noreply.github.com>
2026-05-27 21:35:07 +0300

    Update index4.html

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <?php
 /* This will give an error. Note the output
  * above, which is before the header() call */
-header('Location: http://www.example.com/');
+header('Location: http://www.shein.com/');
 exit;
 ?>
